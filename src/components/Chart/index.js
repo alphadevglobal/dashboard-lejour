e29e8f1d81167a5d8f5076e0f66a47fdd8a23ba2
Mateus <mateus.liderprojetos@pm.me>
2020-11-18 12:38:05 -0300

    ajustes para deploy funfar

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 import { useTheme } from '@material-ui/core/styles';
 import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
 import Title from '../Title';
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -73,7 +72,7 @@ export default function SignIn() {
             control={<Checkbox value="remember" color="primary" />}
             label="Lembrar de mim"
           />
-          <Link href="/dashboard">
+          <Link to="/dashboard">
             <Button
               type="button"
               fullWidth
@@ -86,12 +85,12 @@ export default function SignIn() {
           </Link>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link to="#" variant="body2">
                 Esqueceu a senha?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="/signup" variant="body2">
+              <Link to="/signup" variant="body2">
                 {"Não tem uma conta? Cadastre-se"}
               </Link>
             </Grid>
